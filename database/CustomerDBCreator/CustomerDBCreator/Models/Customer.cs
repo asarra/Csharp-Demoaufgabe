8a825298fa8db434646e191fdbcb5323c5c2ed86
asarra <20255127+asarra@users.noreply.github.com>
2023-11-03 20:13:34 +0100

    db

---
+++ database/CustomerDBCreator/CustomerDBCreator/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerDBCreator.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public int Age { get; set; }
+        public required string Address { get; set; }
+    }
+}
---
+++ database/CustomerDBCreator/CustomerDBCreator/Models/CustomerContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomerDBCreator.Models
+{
+    public class CustomerContext : DbContext
+    {
+        public IConfiguration Configuration { get; }
+
+        public CustomerContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+
+            Configuration = configuration;
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("Default"));
+        }
+
+        // protected override OnConfi
+
+        DbSet<Customer> Customers { get; set;}
+
+    }
+}
